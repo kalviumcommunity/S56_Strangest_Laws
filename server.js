8ff1beb16a69c37d58ddcb270f4280070bc47727
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-02-19 11:26:24 +0530

    Changes made according to feedback

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get('/ping',(req,res)=>{
-  res.send("Pong")
+    res.status(200).send("Pong")
 })
 
 if (require.main === module) {
   app.listen(port, () => {
-    console.log(`server running on ${port}`);
+    console.log(`Server running on port ${port}`);
   });
 }
 
