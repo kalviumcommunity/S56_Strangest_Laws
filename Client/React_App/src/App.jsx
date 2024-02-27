d58a068dda8e3e4dd32ac5839fa265a4889ed5ae
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-02-27 15:43:48 +0530

    added react to the project

---
+++ Client/React_App/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+        </div>
+        <div className="options">
+          <a href="#">Option 1</a>
+          <a href="#">Option 2</a>
+          <a href="#">Option 3</a>
+        </div>
+      </nav>
+
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
