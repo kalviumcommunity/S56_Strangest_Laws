f837a9b9da391f6f0f7b615d8be307f41a87ec38
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-03-08 17:51:05 +0530

    handled server error

--- Server/server.js
+++ Server/server.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const Router = require('./routes.js'); 
-const { isConnected, connected } = require('./db.js'); 
-const cors=require('cors')
+const express = require("express");
+const Router = require("./routes.js");
+const { getDB, mongooseConnect } = require("./db.js");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
-app.get('/', (req, res) => {
-    try {
-        res.json({
-            database: isConnected() ? 'connected' : 'disconnected'
-        });
-    } catch (err) {
-        console.log(err);
-    }
+app.use(express.json());
+app.get("/", (req, res) => {
+  try {
+    res.json({
+      database: isConnected() ? "connected" : "disconnected",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.use(Router);
 
-app.get('/ping', (req, res) => {
-    try {
-        res.send("Pong");
-    } catch (err) {
-        console.log(err);
-    }x1
+app.get("/ping", (req, res) => {
+  try {
+    res.send("Pong");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 if (require.main === module) {
-    connected()
-    app.listen(port, async () => {
-        console.log(`🚀 server running on PORT: ${port}`);
-    });
+  getDB();
+  console.log(mongooseConnect());
+  app.listen(port, async () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
 }
