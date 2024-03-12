8d84c73cead7a0cb44c37fc0ca82300e35cd1953
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-03-12 14:19:32 +0530

    errors handled

--- Client/React_App/src/Components/LoginPage.jsx
+++ Client/React_App/src/Components/LoginPage.jsx
@@ -11,40 +11,39 @@ function LoginPage() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const userNameCookie = getCookie('userName');
-        const passwordCookie = getCookie('password');
-        if (userNameCookie && passwordCookie) {
+        const token = getCookie('token');
+        if (token) {
             setLoggedIn(true);
         }
     }, []);
 
 
 
-    const handleLogin = () => {
-        if (username !== '' && password !== '') {
-            axios.post("https://strangest-laws.onrender.com/auth", {
-                username: username
-            }).then((res) => {
-                const token = res.data.token;
+    const handleLogin = async () => {
+        console.log('username:', username);
+        try {
+            if (username !== '' && password !== '') {
+                const response = await axios.post("https://strangest-laws.onrender.com/auth", {
+                    username: username
+                });
 
-                if (token) {
-                    document.cookie = `token=${token}; expires=Fri, 1 April 2799 12:00:00 UTC;`;
-                    alert('Login Successful.!!');
-                    setTimeout(() => {
-                        navigate('/');
-                        setLoggedIn(true);
-                    }, 10);
-                }
-            }).catch((error) => {
-                console.error('Error during login:', error);
-                alert('An error occurred during login. Please try again.');
-            });
-        } else {
-            alert('UserName and Password are Required!!');
+                console.log('Login:', response);
+
+                const token = response.data;
+                document.cookie = `token=${token}; expires=Fri, 1 April 2799 12:00:00 UTC; path=/;`;
+                alert('Login Successful.!!');
+                setTimeout(() => {
+                    navigate('/');
+                    setLoggedIn(true);
+                }, 10);
+            } else {
+                alert('UserName and Password are Required!!');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred during login. Please try again.');
         }
     };
-
-
 
     const getCookie = (name) => {
         const value = `; ${document.cookie}`;
@@ -71,7 +70,7 @@ function LoginPage() {
                     <label>Enter UserName : </label>
                     <input type="text" onChange={(e) => setUserName(e.target.value)} />
                     <label>Enter Password : </label>
-                    <input type="password" onChange={(e) => setPassword(e.target.value)} />
+                    <input type="pass" onChange={(e) => setPassword(e.target.value)} />
                     <button className="login-btn" onClick={handleLogin}>
                         Login
                     </button>
